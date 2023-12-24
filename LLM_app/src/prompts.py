de6bbc8d71db94ff7748835b3ffad00331b8dcfa
Khai Huyen Nguyen <50721063+Khaihuyennguyen@users.noreply.github.com>
2023-12-24 16:57:52 -0600

    prompt

--- LLM_app/src/prompts.py
+++ LLM_app/src/prompts.py
@@ -16,12 +16,14 @@ from langchain.prompts import (
 logger = logging.getLogger(__name__)
 
 def load_chat_prompt(f_name: Union[pathlib.Path, str] = None) -> ChatPromptTemplate:
+    # A union can be used as Union[A, B] 
+    # to indicate that the object can have type A or B.
     if isinstance(f_name, str) and f_name:
         f_name = pathlib.Path(f_name)
     if f_name and f_name.is_file():
         template = json.loads(f_name.open("r"))
     else:
-        logger.warning(f'No chat prompt provided. Using default chat prompt from {__name}')
+        logger.warning(f'No chat prompt provided. Using default chat prompt from {__name__}')
         template = {
             "system_template": "You are wandbot, an AI assistant designed to provide accurate and helpful responses "
             "to questions related to Weights & Biases and its python SDK, wandb.\nYour goal is to "
@@ -79,3 +81,30 @@ def load_chat_prompt(f_name: Union[pathlib.Path, str] = None) -> ChatPromptTempl
         ]
         promt = ChatPromptTemplate.from_messages(messages)
         return prompt
+    
+def load_eval_prompt(f_name: Union[pathlib.Path, str] = None) -> ChatPromptTemplate:
+    if isinstance(f_name, str) and f_name:
+        f_name = pathlib.Path(f_name)
+    if f_name and f_name.is_file():
+        human_template = f_name.open("r").read()
+    else:
+        logger.warning(
+            f'No human prompt provided. Using default human prompt from {__name__}'
+        )
+        human_template =  """\nQUESTION: {query}\nCHATBOT ANSWER: {result}\n
+        ORIGINAL ANSWER: {answer} GRADE:"""
+        
+    system_message_prompt = SystemMessagePromptTemplate.from_template(
+    """You are an evaluator for the W&B chatbot.You are given a question, the chatbot's answer, and the original answer, 
+    and are asked to score the chatbot's answer as either CORRECT or INCORRECT. Note 
+    that sometimes, the original answer is not the best answer, and sometimes the chatbot's answer is not the 
+    best answer. You are evaluating the chatbot's answer only. Example Format:\nQUESTION: question here\nCHATBOT 
+    ANSWER: student's answer here\nORIGINAL ANSWER: original answer here\nGRADE: CORRECT or INCORRECT here\nPlease 
+    remember to grade them based on being factually accurate. Begin!"""
+    )
+    human_message_prompt = HumanMessagePrompttemplate.from_template(human_template)
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
+    
+    return chat_prompt
